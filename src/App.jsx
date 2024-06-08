ec955126b188e83772bea3036f882555c6b111d6
Matthew <matthewp2718@gmail.com>
2024-06-08 18:17:49 +0200

    update favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
 function App() {
   return (
-    <div className="font-redditMono p-8 text-sm font-semibold">
-      <h1 className="text-3xl">hi, i'm david 👋</h1>
+    <div className="p-8 text-sm font-semibold font-redditMono">
+      <h1 className="text-3xl">hi, i'm matthew 👋</h1>
       <p className="pt-3 font-medium">
         i'm currently building an awesome company with awesome people.
       </p>
       <p className="pt-1">
-        <a
-          href="https://x.com/davidwrossiter"
-          className="underline text-blue-700"
-          target="_blank"
-        >
-          twitter
-        </a>{" "}
         /{" "}
         <a
-          href="https://github.com/davidwrossiter?tab=overview&from=2024-06-01&to=2024-06-08"
-          className="underline text-blue-700"
+          href="https://github.com/matthewp2718"
+          className="text-blue-700 underline"
           target="_blank"
         >
           github
         </a>{" "}
         /{" "}
         <a
-          href="https://www.linkedin.com/in/davidwrossiter/"
-          className="underline text-blue-700"
+          href="https://www.linkedin.com/in/matthew-r-powell/"
+          className="text-blue-700 underline"
           target="_blank"
         >
           linkedin
@@ -34,40 +27,22 @@ function App() {
       <p className="mt-6 font-[400] text-xl">projects</p>
 
       <div className="mt-6">
-        <p className="font-bold text-base">rabbitcode</p>
+        <p className="text-base font-bold">rabbitcode</p>
         <p className="max-w-[500px] mt-1 font-[400]">coming soon...</p>
       </div>
 
       <div className="mt-6">
-        <p href="#" className="font-bold text-base">
-          auth messiah
-        </p>
+        <p className="text-base font-bold">chapter one</p>
         <p className="pt-1">
-          <a href="#" className="underline text-blue-700" target="_blank">
-            github repo
-          </a>{" "}
-        </p>
-        <p className="max-w-[500px] mt-3 font-[350]">
-          this is an open source project that i created with my co-founder joey.
-          we were struggling to find a good way to roll out auth in our apps, so
-          we decided to create an open source auth system that anyone can fork.
-          all you need to do is change the database connections and API keys,
-          link the forked repo to a cloudflare worker and its done.
-        </p>
-      </div>
-
-      <div className="mt-6">
-        <p className="font-bold text-base">chapter one</p>
-        <p className="pt-1">
-          <a href="#" className="underline text-blue-700" target="_blank">
+          <a href="#" className="text-blue-700 underline" target="_blank">
             live project
           </a>{" "}
           /{" "}
-          <a href="#" className="underline text-blue-700" target="_blank">
+          <a href="#" className="text-blue-700 underline" target="_blank">
             github for client
           </a>{" "}
           /{" "}
-          <a href="#" className="underline text-blue-700" target="_blank">
+          <a href="#" className="text-blue-700 underline" target="_blank">
             github for server
           </a>
         </p>
@@ -75,31 +50,6 @@ function App() {
           enter the job that you are trying to get, upload your resume/cv and
           get instant feedback and guidance on how you should optimise your
           resume/cv for the job you are looking to get.
-        </p>
-      </div>
-
-      <div className="mt-6">
-        <p className="font-bold text-base">orin design co.</p>
-        <p className="pt-1">
-          <a
-            href="https://orindesign.co/"
-            className="underline text-blue-700"
-            target="_blank"
-          >
-            live project
-          </a>{" "}
-          /{" "}
-          <a
-            href="https://github.com/davidwrossiter/orin"
-            className="underline text-blue-700"
-            target="_blank"
-          >
-            github for client
-          </a>{" "}
-        </p>
-        <p className="max-w-[500px] mt-3 font-[350]">
-          orin design is my dad's company, i created the website for him in
-          react, and integrated it with stripe so that he can accept payments.
         </p>
       </div>
     </div>
